543c168563acb51bc530e597aee4330a195cd842
Maria Warnes <mlw05@hotmail.co.uk>
2024-03-27 17:26:32 +0000

    Added Tailwind official plugins

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,6 +19,11 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/container-queries'),
+  ]
 };
 export default config;
